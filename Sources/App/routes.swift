a0b948cc8e87d570046762d42fe5fa79ec2ba903
Dan Halliday <dan@halliday.work>
2022-06-14 23:59:34 +0100

    Update to global client

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,14 +1,32 @@
 import Vapor
 import Apollo
 
+struct Network {
+    static var shared = Network()
+
+    private(set) lazy var apollo: ApolloClient = {
+        let url = URL(string: "https://api.spacex.land/graphql/")!
+        let configuration = URLSessionConfiguration.ephemeral
+        let store = ApolloStore(cache: InMemoryNormalizedCache())
+        let client = URLSessionClient(sessionConfiguration: configuration, callbackQueue: .main)
+        let provider = DefaultInterceptorProvider(client: client, store: store)
+        let transport = RequestChainNetworkTransport(interceptorProvider: provider, endpointURL: url)
+
+        return ApolloClient(networkTransport: transport, store: store)
+    }()
+}
+
+extension DispatchQueue {
+    static let apollo = DispatchQueue(label: "apollo")
+}
+
 func routes(_ app: Application) throws {
     app.get { req async throws -> String in
-        await withCheckedContinuation { continuation in
+        return await withCheckedContinuation { continuation in
             print("Servicing request")
-            let client = ApolloClient(url: URL(string: "https://api.spacex.land/graphql/")!)
 
-            client.fetch(query: LaunchesQuery()) { response in
-                print("Returning response (never called)")
+            Network.shared.apollo.fetch(query: LaunchesQuery(), queue: .apollo) { response in
+                print("Returning response")
                 continuation.resume(returning: "Hello World!")
             }
         }
